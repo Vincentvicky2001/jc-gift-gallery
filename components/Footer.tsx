50611f79a15dd8713dc8fefe3e4283afa379f869
charant080 <charant080@gmail.com>
2026-06-10 20:22:01 +0530

    Fixed footer logo path

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         {/* LOGO */}
         <div className="flex justify-center mb-5">
           <img
-            src="/logo.jpg"
+            src="/images/logo.jpg"
             alt="JC Gift Gallery"
             className="w-[280px] md:w-[360px] object-contain"
           />
